e36b80912876e292f541cb73a5d4bd253d70a7b6
bbpak <brianbpak@gmail.com>
2019-03-08 13:03:35 -0800

    more shuffling of questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,69 @@
-TriviaApi.get_questions(amount: "50", category: 9, difficulty: "easy").shuffle.each do |question|
-  Question.create(question)
-end
-TriviaApi.get_questions(amount: "50", category: 18, difficulty: "easy").shuffle.each do |question|
-  Question.create(question)
-end
-TriviaApi.get_questions(amount: "50", category: 16, difficulty: "easy").shuffle.each do |question|
-  Question.create(question)
-end
-TriviaApi.get_questions(amount: "50", category: 23, difficulty: "easy").shuffle.each do |question|
-  Question.create(question)
-end
-TriviaApi.get_questions(amount: "50", category: 27, difficulty: "easy").shuffle.each do |question|
-  Question.create(question)
-end
-
-TriviaApi.get_questions(amount: "50", category: 9, difficulty: "medium").shuffle.each do |question|
-  Question.create(question)
-end
-TriviaApi.get_questions(amount: "50", category: 10, difficulty: "medium").shuffle.each do |question|
-  Question.create(question)
-end
-TriviaApi.get_questions(amount: "50", category: 22, difficulty: "medium").shuffle.each do |question|
-  Question.create(question)
-end
-
-TriviaApi.get_questions(amount: "45", category: 9, difficulty: "hard").shuffle.each do |question|
-  Question.create(question)
-end
-TriviaApi.get_questions(amount: "40", category: 17, difficulty: "hard").shuffle.each do |question|
-  Question.create(question)
-end
-TriviaApi.get_questions(amount: "15", category: 22, difficulty: "hard").shuffle.each do |question|
-  Question.create(question)
+(1..5).to_a.sample(5).each do |idx|
+  case idx
+  when 1
+    TriviaApi.get_questions(amount: "50", category: 9, difficulty: "easy").shuffle.each do |question|
+      Question.create(question)
+    end
+  when 2
+    TriviaApi.get_questions(amount: "50", category: 18, difficulty: "easy").shuffle.each do |question|
+      Question.create(question)
+    end
+  when 3
+    TriviaApi.get_questions(amount: "50", category: 16, difficulty: "easy").shuffle.each do |question|
+      Question.create(question)
+    end
+  when 4
+    TriviaApi.get_questions(amount: "50", category: 23, difficulty: "easy").shuffle.each do |question|
+      Question.create(question)
+    end
+  when 5
+    TriviaApi.get_questions(amount: "50", category: 27, difficulty: "easy").shuffle.each do |question|
+      Question.create(question)
+    end
+  else
+    TriviaApi.get_questions(amount: "50", category: 9, difficulty: "easy").shuffle.each do |question|
+      Question.create(question)
+    end
+  end
 end
 
+(1..3).to_a.sample(3).each do |idx|
+  case idx
+  when 1
+    TriviaApi.get_questions(amount: "50", category: 9, difficulty: "medium").shuffle.each do |question|
+      Question.create(question)
+    end
+  when 2
+    TriviaApi.get_questions(amount: "50", category: 22, difficulty: "medium").shuffle.each do |question|
+      Question.create(question)
+    end
+  when 3
+    TriviaApi.get_questions(amount: "50", category: 10, difficulty: "medium").shuffle.each do |question|
+      Question.create(question)
+    end
+  else
+    TriviaApi.get_questions(amount: "50", category: 9, difficulty: "medium").shuffle.each do |question|
+      Question.create(question)
+    end
+  end
+end
+
+(1..3).to_a.sample(3).each do |idx|
+  case idx
+  when 1
+    TriviaApi.get_questions(amount: "45", category: 9, difficulty: "hard").shuffle.each do |question|
+      Question.create(question)
+    end
+  when 2
+    TriviaApi.get_questions(amount: "40", category: 17, difficulty: "hard").shuffle.each do |question|
+      Question.create(question)
+    end
+  when 3
+    TriviaApi.get_questions(amount: "15", category: 22, difficulty: "hard").shuffle.each do |question|
+      Question.create(question)
+    end
+  end
+end
 
 b = User.find_or_create_by(name: "Beary White")
 j = User.find_or_create_by(name: "Ursa Funny")
